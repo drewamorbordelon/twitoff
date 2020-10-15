40306db4ee4edd22e700042e8193bc6ca5d7f24c
drewamorbordelon <drew-bordelon@lambdastudents.com>
2020-10-15 13:14:50 -0500

    Getting ready to deploy of Heroku

--- twitoff/__init__.py
+++ twitoff/__init__.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-"""Entry point for Twitoff"""
-=======
-"""Entry point for TwitOff"""
->>>>>>> 5899639952cb839c499ce384ab140b01d926800f
 from .app import create_app
 
 APP = create_app()
