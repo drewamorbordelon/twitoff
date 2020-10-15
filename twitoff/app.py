0c804f90b9d809e6b8bba22f1fc6564c122269a8
drewamorbordelon <drew-bordelon@lambdastudents.com>
2020-10-15 10:44:36 -0500

    updated .py files for Twitoff Application

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,48 +1,84 @@
-"""Main app/routing file for TwitOff"""
+"""Main app/routig file for Twitoff"""
 
-from flask import Flask, render_template
-from .models import DB, User, insert_example_users
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import add_or_update_user, update_all_users
+from os import getenv
+from .predict import predict_user
+
 
 
 def create_app():
-    """Create and configure an instance of the Flask application"""
+    """ Creates and Configures a Flask application"""
 
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3" # where DB is stored
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
-    # ... TODO make the app
-    @app.route('/')  # Decorator = extend and modify behavior of a class, method, function
+
+    @app.route('/')
     def root():
-        insert_example_users()  #  calls function within models.py - insert users
         return render_template("base.html", title="Home", users=User.query.all())
 
-    # when you run the application we haven't built any other
-    # functionality other than the users showing up so keep that in mind    
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        # grabs inputted values from the dropdown 
+        user0, user1 = sorted(
+            [request.values['user1'],
+             request.values['user2']]
+        )
+
+        if user0 == user1:
+            # tells application user they cant compare same twitter users
+            message = "Cannot compare users to themselves!"
+
+        else:
+            # running prediction and return the prediction to user as a message
+            prediction = predict_user(user0, user1, request.values['tweet_text'])
+            message = "{} is more likely to be said by {} than {}".format(
+                request.values['tweet_text'], user1 if prediction else user0,
+                user0 if prediction else user1)
+
+        return render_template('prediction.html', title='Prediction', message=message)
+
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        # either grab a user that already exist in our DB or grab the users input
+        name = name or request.values['user_name']
+
+        try:
+            # if button is clicked then do this
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} sucessfully added!'.format(name)
+            # tweets are always collected if the user exist
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            # if we get an error then no tweets are displayed
+            tweets=[]
+
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
+
     @app.route('/update')
     def update():
-        #  Reset the database 
-        DB.drop_all()  #  Deletes already present databases
-        DB.create_all()  #  Creates the database from scratch
-        insert_example_users()
-        return render_template('base.html', title="Users updated!", users=User.query.all())
+        # updates our users from the function in twitter.py
+        update_all_users()
+        return render_template('base.html', title="Tweets have been updated!", users=User.query.all())
+
+
+    @app.route('/reset')
+    def reset():
+        # resets database
+        DB.drop_all()
+        # creates database again
+        DB.create_all()
+        return render_template('base.html', title='Reset Database!')
+
 
     return app
-
-
-
-    
-
-    # """This is the Static form"""
-    # from flask import Flask
-
-    # def create_app():
-    #     """Create an configure an instance of the Flask application"""
-    #     app = Flask(__name__)
-
-    #     @app.route('/')
-    #     def root():
-    #         return "Hello!"
-
-    #     return app
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -2,29 +2,62 @@
 import tweepy
 import spacy
 from .models import DB, Tweet, User
+from os import getenv
 
-
-TWITTER_API_KEY = 'tfw9TiPzSmPdVTkSowgAPPY5D'
-TWITTER_API_KEY_SECRET = 'uQw9MgqT6eLWO3imL3sKSsD8klAtF5qfLxHDtwVE5r1mnp1ALE'
+# Grabbing from your own .env file
+TWITTER_API_KEY = getenv('TWITTER_API_KEY')
+TWITTER_API_KEY_SECRET = getenv('TWITTER_API_KEY_SECRET')
 TWITTER_AUTH = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_KEY_SECRET)
 TWITTER = tweepy.API(TWITTER_AUTH)
 
 
-# Create a function to grab a USER twitter handle
+# for turning our tweets into an array of numbers
+nlp = spacy.load('my_model') # loaded from my_models dir
+def vectorize_tweet(tweet_text):
+  return nlp(tweet_text).vector
+
 def add_or_update_user(username):
-    """Allows to add/update users to our database"""
+  try:
+    """Allows us to add/update users to our DB"""
     twitter_user = TWITTER.get_user(username)
-    db_user = (User.query.get(twitter_user.id)) or User(id=twitter_user.ide, name=username)
+    # either updates or adds a user depending upon if they are in the DB
+    db_user = (User.query.get(twitter_user.id)) or User(id=twitter_user.id, name=username)
     DB.session.add(db_user)
 
     tweets = twitter_user.timeline(
-        count=200, exclude_replies=True,
-        include_rts=False, tweet_mode='extended'
+      count=200, exclude_replies=True,
+      include_rts=False, tweet_mode='extended',
+      since_id=db_user.newest_tweet_id
     )
 
+    # will update the most recent tweet id to the user
+    if tweets: 
+      db_user.newest_tweet_id = tweets[0].id
     for tweet in tweets:
-        db_tweet = Tweet(id=tweet.id, text=tweet.full_text)
-        db_user.tweets.append(db_tweet)
-        DB.session.add(db_tweet)
+      vectorized_tweet = vectorize_tweet(tweet.full_text)
+      db_tweet = Tweet(
+        id=tweet.id, text=tweet.full_text,
+        vect=vectorized_tweet
+      )
+      db_user.tweets.append(db_tweet)
+      DB.session.add(db_tweet)
 
+  except Exception as e:
+    print('Error Processing {}: {}'.format(username, e)) # gives an error
+    raise e
+
+  # last thing done is committing changes
+  else: 
     DB.session.commit()
+
+# a function used to update our users when the update button is clicked
+def update_all_users():
+  """Update all Tweets for all Users in the User Table."""
+  for user in User.query.all():
+    add_or_update_user(user.name)
+
+# def insert_example_users():
+#   # using our functions to add two users
+#   add_or_update_user('hillaryclinton')
+#   add_or_update_user('jackblack')
+
