4903a39bfc25be252e504d2521b9b7de7437dfea
drewamorbordelon <drew-bordelon@lambdastudents.com>
2020-10-15 12:08:58 -0500

    Prepping for Deploying to Heroku

---
+++ twitoff/models.py
@@ -0,0 +1,41 @@
+"""SQLAlchemy models and utility fucntions for TwitOff"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+DB = SQLAlchemy()
+
+
+# User table with columns id and name
+class User(DB.Model):
+  """Twitter Users corresponding to Tweets"""
+  id = DB.Column(DB.BigInteger, primary_key=True)
+  name = DB.Column(DB.String, nullable=False)
+  newest_tweet_id = DB.Column(DB.BigInteger)
+
+  def __repr__(self):
+    return "<User: {}>".format(self.name)
+
+
+# Tweet table with columns id, text, and user_id
+class Tweet(DB.Model):
+  """Tweet related to a user"""
+  id = DB.Column(DB.BigInteger, primary_key=True)
+  text = DB.Column(DB.Unicode(300))
+  vect = DB.Column(DB.PickleType, nullable=False)
+  user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+  user = DB.relationship("User", backref=DB.backref("tweets", lazy=True))
+
+  def __repr__(self):
+    return "<Tweet: {}>".format(self.text)
+
+
+#  Example users but remember they don't have tweets
+# def insert_example_users():
+#     """Example Users"""
+#     bill = User(id=1, name="BillGates")
+#     elon = User(id=2, name="ElonMusK")
+
+#     DB.session.add(bill)
+#     DB.session.add(elon)
+#     DB.session.commit()
